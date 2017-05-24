fe274e5d39f4fbdd2548dd597bf2f000f93b9ad4
Geraldine Granada <gkgranada@gmail.com>
2017-05-24 19:56:44 +0300

    Add role based access

---
+++ src/main/java/com/maintenance/common/security/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.maintenance.common.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableWebMvc
+@EnableGlobalMethodSecurity(securedEnabled=true)
+public class SecurityConfiguration {
+    @Autowired
+    @Qualifier("dataSource")
+    DataSource dataSource;
+
+    @Autowired
+    void configureAuthenticationSystem(AuthenticationManagerBuilder builder) throws Exception {
+         builder
+                 .jdbcAuthentication()
+                 .dataSource(dataSource)
+                 .usersByUsernameQuery(
+                         "select username,password,enabled from users where username=?")
+                 .authoritiesByUsernameQuery(
+                     "select username,authority from authorities where username=?")
+                 .and()
+                 .inMemoryAuthentication()
+                 .withUser("user2").password("user2").roles("USER2");
+
+    }
+
+    @Configuration
+    class RestSecurityConfiguration extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.cors().and().csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers(HttpMethod.OPTIONS, "/api/**").permitAll()
+                    .antMatchers("/api/**").authenticated()
+                    .and().httpBasic()
+                    .authenticationEntryPoint((req,res,exc) ->
+                        res.sendError(HttpServletResponse.SC_UNAUTHORIZED, "You don't have anything to see here"));
+            http.headers().frameOptions().sameOrigin();
+        }
+    }
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+create table if not exists users (username varchar(50) not null primary key, password varchar(50) not null, enabled boolean not null)
+create table if not exists authorities (username varchar(50) not null, authority varchar(50) not null, constraint FK_AUTHORITIES_USERS foreign key(username) references users(username))
+
+insert into users (username, password, enabled) values ('admin', 'admin', true);
+insert into authorities (username, authority) values ('admin', 'ROLE_ADMIN');
+insert into users (username, password, enabled) values ('siteengr', 'siteengr', true);
+insert into users (username, password, enabled) values ('worksengr', 'worksengr', true);
+insert into users (username, password, enabled) values ('supervisor', 'supervisor', true);
+insert into authorities (username, authority) values ('siteengr', 'ROLE_SITE_ENGINEER');
+insert into authorities (username, authority) values ('worksengr', 'ROLE_WORKS_ENGINEER');
+insert into authorities (username, authority) values ('supervisor', 'ROLE_SITE_ENGINEER');
+insert into authorities (username, authority) values ('supervisor', 'ROLE_WORKS_ENGINEER');
